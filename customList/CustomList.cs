ec8f5846c6c6798c91ef75a8622f8912ab510b57
EricBMielke <51930708+EricBMielke@users.noreply.github.com>
2019-07-17 11:04:40 -0500

    First commit with the initial set of unit tests that are needed to begin practicing TDD with the add method

---
+++ customList/CustomList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    public class CustomList<T>
+    {
+        public int capacity;
+        readonly int count;
+        //just get, no set
+        public void Add(T itemToAdd)
+        {
+
+        }
+    }
+}
