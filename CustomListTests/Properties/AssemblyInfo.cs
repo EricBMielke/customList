6904ae7811bcc589551b37ae19f0a5aa1858e2a3
EricBMielke <51930708+EricBMielke@users.noreply.github.com>
2019-07-17 11:07:31 -0500

    Adding the proper structure with the unit tests and solution

---
+++ CustomListTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomListTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomListTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("53e3d250-9783-4525-9bd6-ea5d5b4a6d81")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CustomListTests/UnitTest1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using CustomList;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CustomListTests
+{
+    [TestClass]
+    public class CustomListTests
+    {
+        // if i add something to a populated customlist, it should go to last index
+        [TestMethod]
+        public void Add_AddToPopulatedList_ItemShouldGoToLastIndex()
+        {
+            // arrange
+            CustomList<int> test = new CustomList<int>();
+            int expected = 10;
+            int actual;
+            // act
+            test.Add(8);
+            test.Add(10);
+            actual = test[test.Count];
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+        // if i add something to customlist whose inner array is full, it should still add the item
+        [TestMethod]
+        public void Add_AddToFullList_ItemShouldBeAdded()
+        {
+            // arrange
+            CustomList<int> test = new CustomList<int>();
+            int expected = 16;
+            int actual;
+            // act
+            test.Add(8);
+            test.Add(10);
+            test.Add(12);
+            test.Add(14);
+            test.Add(16);
+            actual = test[test.Count];
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Add_AddToEmptyList_ItemShouldGoToIndexZero()
+        {
+            // arrange
+            CustomList<int> test = new CustomList<int>();
+            int expected = 8;
+            int actual;
+            // act
+            test.Add(8);
+            actual = test[0];
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Add_AddToEmptyList_CountShouldIncreaseToOne()
+        {
+            // arrange
+            CustomList<int> test = new CustomList<int>();
+            int expected = 1;
+            int actual;
+            // act
+            test.Add(8);
+            actual = test.Count;
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
